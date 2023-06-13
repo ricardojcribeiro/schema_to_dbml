a582312cf9a98b23ad119a03b36172bab9ec93a5
Daniel Veloso <danielveloso09@gmail.com>
2023-06-13 17:33:46 +0100

    fix: columns regex to accept options in different order (#24)
    Co-authored-by: Ricardo Ribeiro <ricardo.ribeiro@knokcare.com>

--- lib/schema_to_dbml/helpers/constants.rb
+++ lib/schema_to_dbml/helpers/constants.rb
@@ -2,12 +2,32 @@
 
 module Helpers
   module Constants
-    # rubocop:disable Layout/LineLength
-    TABLES_REGEXP = /create_table\s+"(?<table_name>\w+)"(?:,\s+comment:\s+"(?<comment>.*?)")?(?:,\s+force:\s+:cascade)?\s+do\s+\|t\|\n(?<table_content>(?:.*?)(?:".*?")*.*?)(?<=\n)\s+end/m
+    TABLES_REGEXP = /
+      create_table\s+"(?<table_name>\w+)"
+      (?:,\s+comment:\s+"(?<comment>.*?)")?
+      (?:,\s+force:\s+:cascade)?
+      \s+do\s+\|t\|
+      \n
+      (?<table_content>(?:.*?)(?:".*?")*.*?)
+      (?<=\n)
+      \s+end
+    /xm
 
-    COLUMNS_REGEXP = /t\.(?<type>\w+)\s+"(?<name>\w+)"(?:,\s+(?<default>default:[^,\n]+?(?=(?:,\s)|$)))?(?:,\s+(?<null>null:\s+\w+))?(?:,\s+comment:\s+"(?<comment>[^"]+)")?(?:,\s+precision:\s+(?<precision>\d+))?(?:,\s+array:\s+(?<array>true|false))?.*/
+    COLUMNS_REGEXP = /
+      t\.(?<type>\w+)\s+"(?<name>\w+)"
+      (?:
+        (?:,\s+(?<default>default:[^,\n]+?(?=(?:,\s)|$)))?
+        (?:,\s+(?<null>null:\s+\w+))?
+        (?:,\s+comment:\s+"(?<comment>[^"]+)")?
+        (?:,\s+precision:\s+(?<precision>\d+))?
+        (?:,\s+array:\s+(?<array>true|false))?
+      )*
+    /x
 
-    RELATIONS_REGEXP = /add_foreign_key\s+"(?<from_table>\w+)",\s+"(?<to_table>\w+)"(?:,\s+column:\s+"(?<column>\w+)")?(?:,\s+on_delete:\s+:(?<on_delete>\w+))?/
-    # rubocop:enable Layout/LineLength
+    RELATIONS_REGEXP = /
+      add_foreign_key\s+"(?<from_table>\w+)",\s+"(?<to_table>\w+)"
+      (?:,\s+column:\s+"(?<column>\w+)")?
+      (?:,\s+on_delete:\s+:(?<on_delete>\w+))?
+    /x
   end
 end
