da326c5d0c9e55eaa7be9623d23ad1369bd810db
Daniel Veloso <danielveloso09@gmail.com>
2024-01-30 17:46:06 +0000

    chore: add undercover ci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'simplecov-lcov'
 
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::LcovFormatter
+])
 SimpleCov.start do
   add_filter '/spec/'
 end
