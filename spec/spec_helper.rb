0bf508823b96189a18b3f7eb08417f3d61105779
Daniel Veloso <danielveloso09@gmail.com>
2024-02-15 13:10:18 +0000

    refactor: move helpers to support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,9 +19,9 @@ require 'byebug'
 require 'schema_to_dbml'
 require_relative '../lib/schema_to_dbml/errors/schema_file_not_found_error'
 require_relative '../lib/schema_to_dbml/errors/configuration_file_not_found_error'
-require_relative '../spec/lib/schema_to_dbml/support/schema_converter_spec_helper'
-require_relative '../spec/lib/schema_to_dbml/support/constants_spec_helper'
-require_relative '../spec/lib/schema_to_dbml/support/final_dbml_content_spec_helper'
+require_relative '../spec/support/schema_converter_spec_helper'
+require_relative '../spec/support/constants_spec_helper'
+require_relative '../spec/support/final_dbml_content_spec_helper'
 
 SUPPORT_FILES_PATH = File.join(File.dirname(__FILE__), 'support', 'files')
 
