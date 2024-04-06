79e2eff7bb29345cc7033fb7f0cd0b9a8ece613c
Daniel Veloso <danielveloso09@gmail.com>
2024-04-06 14:34:28 +0100

    fix: please rubocop

--- spec/lib/rake_tasks/schema_to_dbml_spec.rb
+++ spec/lib/rake_tasks/schema_to_dbml_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe 'schema_to_dbml' do
+RSpec.describe 'SchemaToDbml' do
   before do
     Rake::Task.define_task(:environment)
   end
@@ -11,11 +11,13 @@ RSpec.describe 'schema_to_dbml' do
     let(:task) { Rake::Task['schema_to_dbml:generate'] }
     let(:perform) { task.invoke(file_path) }
 
-    it 'delegates proper parameters to SchemaToDbml' do
-      expect(SchemaToDbml)
+    before do
+      allow(SchemaToDbml)
         .to receive(:new)
         .and_return(schema_instance)
+    end
 
+    it 'delegates proper parameters to SchemaToDbml' do
       expect(schema_instance)
         .to receive(:generate)
         .with(schema: file_path)
